c4ec90a198bafb399cec77173209a65bcbb93b45
monuvio <kronizzyd@gmail.com>
2019-01-09 14:00:31 +0500

    Bot

---
+++ model/config.py
@@ -0,0 +1,195 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler
+from telegram.ext import ConversationHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from lang_dict import *
+import logging
+from modelsus import StyleTransferModel
+from telegram_token import token
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from array import array
+import os
+import io
+import torchvision.transforms.functional as F
+import torchvision.transforms as transforms
+import torch
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+LANG = "EN"
+SET_LANG, MENU, SET_STAT, ABOUT, NEURO = range(5)
+STATE = SET_LANG
+
+def start(bot, update):
+    """
+    Start function. Displayed whenever the /start command is called.
+    This function sets the language of the bot.
+    """
+    # Create buttons to select language:
+    keyboard = [['RU', 'EN']]
+
+    # Create initial message:
+    message = "Hey, I'm DisAtBot! / Привет! Я бот для переноса стилей! \n\n\
+Please select a language to start. / Пожалуйста, выбери язык, чтобы начать."
+				
+
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+    update.message.reply_text(message, reply_markup=reply_markup)
+
+    return SET_LANG
+	
+def set_lang(bot, update):
+    """
+    First handler with received data to set language globally.
+    """
+    # Set language:
+    global LANG
+    LANG = update.message.text
+    user = update.message.from_user
+
+    logger.info("Language set by {} to {}.".format(user.first_name, LANG))
+    update.message.reply_text(lang_selected[LANG],
+                              reply_markup=ReplyKeyboardRemove())
+
+    return MENU
+	
+def menu(bot, update):
+    """
+    Main menu function.
+    This will display the options from the main menu.
+    """
+    # Create buttons to select language:
+    keyboard = [[choose_net_variation[LANG], view_about[LANG]]]
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+    user = update.message.from_user
+    logger.info("Menu command requested by {}.".format(user.first_name))
+    update.message.reply_text(main_menu[LANG], reply_markup=reply_markup)
+    return SET_STAT
+	
+def set_state(bot, update):
+    """
+    Set option selected from menu.
+    """
+    # Set state:
+    global STATE
+    user = update.message.from_user
+    if update.message.text == choose_net_variation[LANG]:
+        STATE = NEURO
+        neural_set(bot, update)
+        return MENU
+    elif update.message.text == view_about[LANG]:
+        STATE = ABOUT
+        about_bot(bot, update)
+        return MENU
+    else:
+        STATE = MENU
+        return MENU
+		
+def neural_set(bot, update):
+    """
+    Setting variation of neural net and start style transfer session
+    """
+    user = update.message.from_user
+    logger.info("{} choose neural net.".format(user.first_name))
+    update.message.reply_text(neural_net[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return
+	
+def about_bot(bot, update):
+    """
+    About function. Displays info about DisAtBot.
+    """
+    user = update.message.from_user
+    logger.info("About info requested by {}.".format(user.first_name))
+    bot.send_message(chat_id=update.message.chat_id, text=about_info[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return
+	
+def help(bot, update):
+    """
+    Help function.
+    This displays a set of commands available for the bot.
+    """
+    user = update.message.from_user
+    logger.info("User {} asked for help.".format(user.first_name))
+    update.message.reply_text(help_info[LANG],
+                              reply_markup=ReplyKeyboardRemove())
+	
+def cancel(bot, update):
+    """
+    User cancelation function.
+    Cancel conversation by user.
+    """
+    user = update.message.from_user
+    logger.info("User {} canceled the conversation.".format(user.first_name))
+    update.message.reply_text(goodbye[LANG],
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+	
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+	
+def main():
+    """
+    Main function.
+    This function handles the conversation flow by setting
+    states on each step of the flow. Each state has its own
+    handler for the interaction with the user.
+    """
+    global LANG
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(token, request_kwargs={'proxy_url': 'socks4://168.195.171.42:44880'})
+
+    # Get the dispatcher to register handlers:
+    dp = updater.dispatcher
+
+    # Add conversation handler with predefined states:
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            SET_LANG: [RegexHandler('^(RU|EN)$', set_lang)],
+
+            MENU: [CommandHandler('menu', menu)],
+
+            SET_STAT: [RegexHandler(
+                        '^({}|{})$'.format(
+                            choose_net_variation['RU'], view_about['RU'],),
+                        set_state),
+                       RegexHandler(
+                        '^({}|{})$'.format(
+                            choose_net_variation['EN'], view_about['EN'],),
+                        set_state)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel),
+                   CommandHandler('help', help)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # Log all errors:
+    dp.add_error_handler(error)
+
+    # Start DisAtBot:
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process
+    # receives SIGINT, SIGTERM or SIGABRT:
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ model/lang_dict.py
@@ -0,0 +1,58 @@
+lang_selected = {
+    "RU": "Готово! \n\n\
+Чем могу быть полезен? Мои функции ты можешь найти с помощью команды /menu.",
+    "EN": "All set! \n\n\
+How can I help you? A list of options can be found with the \
+/menu command."
+}
+
+main_menu = {
+    "RU": "Пожайлуйста, выбери одну из функций ниже",
+    "EN": "Please select an option from below:"
+}
+
+choose_net_variation = {
+    "RU": "Выбери, какая сеть будет обрабатывать твои изображения",
+    "EN": "Choose neural net variation"
+}
+
+view_about = {
+    "RU": "О боте",
+    "EN": "About"
+}
+
+back2menu = {
+    "RU": "Ты можешь вернуться обратно в меню с помощью команды /menu.",
+    "EN": "You can go back to menu with the /menu command."
+}
+
+goodbye = {
+    "RU": "Пока! Надеемся увидеть тебя ещё!",
+    "EN": "Bye, hope to chat back soon!"
+}
+
+about_info = {
+    "RU": "Soy un chatbot que ayuda ante las situaciones de desastre. \
+Envíame un reporte con tu localización para alimentar una base de datos \
+y así poder ubicarte geográficamente y poder enviarte ayuda. \n\n\
+Me encunetro en una versión alpha desarrollada por @RodolfoFerro.",
+    "EN": "I am a chatbot that helps in disaster situations. \
+Send me a report with your geolocation to feed a database an be able to \
+geo-locate you in order to send some help. \n\n\
+I'm in an alpha version developed by @RodolfoFerro."
+}
+
+neural_net = {
+    "RU": "Выбери, какой нейросетью ты будешь пользоваться.",
+    "EN": "Choose neural network for styling"
+}
+
+help_info = {
+    "RU": "Используй команду /cancel , чтобы выйти из чата. \nИспользуй /start , чтобы перезагрузить бота.",
+    "EN": "Use /cancel to end chat. \nUse /start to restart StyleTransferBot."
+}
+
+template = {
+    "RU": "",
+    "EN": ""
+}
