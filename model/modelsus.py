fa018774412082fa9ac0688fbe967238c203bbf5
monuvio <kronizzyd@gmail.com>
2019-01-09 10:12:06 +0500

    Update modelsus.py

--- model/modelsus.py
+++ model/modelsus.py
@@ -152,12 +152,8 @@ class ContentLoss(nn.Module):
 
         def __init__(self, target,):
             super(ContentLoss, self).__init__()
-            # we 'detach' the target content from the tree used
-            # to dynamically compute the gradient: this is a stated value,
-            # not a variable. Otherwise the forward method of the criterion
-            # will throw an error.
-            self.target = target.detach()#это константа. Убираем ее из дерева вычеслений
-            self.loss = F.mse_loss(self.target, self.target )#to initialize with something
+            self.target = target.detach()
+            self.loss = F.mse_loss(self.target, self.target )
 
         def forward(self, input):
             self.loss = F.mse_loss(input, self.target)
@@ -177,14 +173,10 @@ class StyleLoss(nn.Module):
 class Normalization(nn.Module):
         def __init__(self, mean, std):
             super(Normalization, self).__init__()
-            # .view the mean and std to make them [C x 1 x 1] so that they can
-            # directly work with image Tensor of shape [B x C x H x W].
-            # B is batch size. C is number of channels. H is height and W is width.
             self.mean = torch.tensor(mean).view(-1, 1, 1)
             self.std = torch.tensor(std).view(-1, 1, 1)
 
         def forward(self, img):
-            # normalize img
             return (img - self.mean) / self.std
 
 
